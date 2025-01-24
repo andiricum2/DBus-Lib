5f609cefe4e7b1219921ed92ffe55f5c3f56f8b0
Andoni <andiricum2@gmail.com>
2025-01-24 21:15:54 +0100

    Update ListadoLineasResponse.java

--- src/main/java/com/andiri/libs/dbus/model/response/ListadoLineasResponse.java
+++ src/main/java/com/andiri/libs/dbus/model/response/ListadoLineasResponse.java
@@ -8,12 +8,12 @@ import java.util.List;
 public class ListadoLineasResponse { // New POJO for listadoLineas
     private String estado;
     private Avisos avisos;
-    private List<Linea> lineas;
+    private List<Linea> lista;
 
     public String getEstado() { return estado; }
     public void setEstado(String estado) { this.estado = estado; }
     public Avisos getAvisos() { return avisos; }
     public void setAvisos(Avisos avisos) { this.avisos = avisos; }
-    public List<Linea> getLineas() { return lineas; }
-    public void setLineas(List<Linea> lineas) { this.lineas = lineas; }
+    public List<Linea> getLista() { return lista; }
+    public void setLista(List<Linea> lista) { this.lista = lista; }
 }
