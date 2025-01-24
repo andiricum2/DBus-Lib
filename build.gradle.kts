3a8a4c22788ef834b30bea0549c4ef0eaed2c80f
Andoni <andiricum2@gmail.com>
2025-01-24 08:55:51 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,8 @@ repositories {
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
-    }
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
     withSourcesJar()
 }
 
